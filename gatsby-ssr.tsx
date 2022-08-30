fc09c115fba97ac0e4dc855870e7f8fa639f735d
WsMonty <grethen.gilles@gmail.com>
2022-08-30 11:25:40 +0200

    fixed bugs

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './src/styles/main.scss';
+import { store, persistor } from './src/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {element}
+      </PersistGate>
+    </Provider>
+  );
+};
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/es/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';

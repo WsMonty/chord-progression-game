83fb1e07248d35195bc9a4289ab92b40dd7de755
WsMonty <grethen.gilles@gmail.com>
2022-09-01 09:34:20 +0200

    added analytics

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
 import { PageProps } from 'gatsby';
+import CookieConsent from 'react-cookie-consent';
 
 const Layout = ({ children }: PageProps) => {
   return (
     <div id="root">
+      <CookieConsent
+        cookieName="Google Analytics"
+        buttonStyle={{ backgroundColor: '##ffd427', fontSize: '13px' }}
+        style={{ background: '#191919' }}
+      />
       <Navbar />
       {children}
     </div>

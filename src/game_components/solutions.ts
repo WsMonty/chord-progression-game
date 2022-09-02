54f7b8bc44e5b44d4ec411163fa2f4def20ee503
WsMonty <grethen.gilles@gmail.com>
2022-09-02 11:25:25 +0200

    added songs

--- src/game_components/solutions.ts
+++ src/game_components/solutions.ts
@@ -3,14 +3,33 @@ interface solutionObjType {
 }
 
 export const solutionObj: solutionObjType = {
-  1: ['IMaj', 'vimin', 'iimin', 'V7', 'iiimin', 'vimin', 'iimin', 'V7'],
-  2: ['IVMaj', 'IVMaj', 'ivmin', 'VII7', 'IMaj', 'IMaj', 'SUBII/II', 'SUBV/II'],
-  3: ['IMaj', 'IMaj', 'V/V', 'V/V', 'iimin', 'V7', 'Imaj', 'IMaj'],
-  4: ['II/III', 'V/III', 'IIIMaj', 'VIMaj', 'iimin', 'V7', 'imin', 'imin'],
-  5: ['IMaj', 'IMaj', 'II/VI', 'V/VI', 'vimin', 'vimin', 'II/IV', 'V/IV'],
-  6: ['iimin', 'V7', 'iimin', 'V7', 'IMaj', 'vimin', 'II/II', 'V/II'],
-  7: ['IMaj', 'V/VI', 'V/V', 'V/IV', 'SUBV/III', 'V/II', 'V/V', 'V7'],
-  8: ['II/IV', 'V/IV', 'ivmin', 'ivmin', 'iimin', 'V7', 'IMaj', 'IMaj'],
-  9: ['imin', 'V7', 'imin', 'imin', 'II/VI', 'V/VI', 'VIMaj', 'VIMaj'],
-  10: ['imin', 'IIMaj', 'V7', 'VIMaj', 'ivmin', 'IIMaj', 'IMaj', 'imin'],
+  1: ['IMaj', 'V/II', 'iimin', 'V7', 'iiimin', 'vimin', 'iimin', 'V7'], // Have you met Miss Jones?
+  2: ['IVMaj', 'IVMaj', 'ivmin', 'VII7', 'IMaj', 'IMaj', 'SUBII/II', 'SUBV/II'], // Just Friends
+  3: ['IMaj', 'IMaj', 'V/V', 'V/V', 'iimin', 'V7', 'Imaj', 'IMaj'], // Take the A Train
+  4: ['II/III', 'V/III', 'IIIMaj', 'VIMaj', 'iimin', 'V7', 'imin', 'imin'], // Autumn Leaves
+  5: ['IMaj', 'IMaj', 'II/VI', 'V/VI', 'vimin', 'vimin', 'II/IV', 'V/IV'], // There will never be another you
+  6: ['iimin', 'V7', 'iimin', 'V7', 'IMaj', 'vimin', 'II/II', 'V/II'], // Scrapple from the apple
+  7: ['IMaj', 'V/VI', 'V/V', 'V/IV', 'SUBV/III', 'V/II', 'V/V', 'V7'], //
+  8: ['II/IV', 'V/IV', 'ivmin', 'ivmin', 'iimin', 'V7', 'IMaj', 'IMaj'], // What is this thing called love
+  9: ['imin', 'V7', 'imin', 'imin', 'II/VI', 'V/VI', 'VIMaj', 'VIMaj'], // You'd be so nice to come home to
+  10: ['imin', 'IIMaj', 'V7', 'VIMaj', 'ivmin', 'IIMaj', 'IMaj', 'imin'], // Nardis
+  11: ['V/V', 'V7', 'IMaj', 'IMaj', 'II/II', 'V/II', 'V/V', 'V7'], // If i were a bell
+  12: ['V/V', 'V7', 'IMaj', 'IMaj', 'II/IV', 'V/IV', 'IVMaj', 'IVMaj'], // In a mellow tone
+  13: ['IMaj7', 'SUBV/III', 'SUBV/VI', 'V/II', 'V/V', 'V/V', 'iimin', 'V7'], // There's no greater love
+  14: ['iimin', 'V7', 'IMaj', 'IVMaj', 'II/VI', 'V/VI', 'vimin', 'SUBV/II'], // Alice in Wonderland
+  15: ['iimin', 'V7', 'imin', 'imin', 'II/III', 'V/III', 'IIIMaj', 'V7'], // Beautiful Love
+  16: [
+    'IMaj7',
+    'SUBV/VI',
+    'II/II',
+    'V/II',
+    'iimin',
+    'iimin',
+    'SUBV/VI',
+    'SUBV/VI',
+  ], // Days of wine and Roses
+  17: ['IMaj', 'vimin', 'iimin', 'V7', 'IMaj', 'V/IV', 'IVMaj', 'SUBV/VI'], // Emily
+  18: ['vimin', 'iimin', 'V7', 'IMaj', 'IVMaj', 'II/VI', 'V/VI', 'vimin'], // Fly me to the moon
+  19: ['IMaj', 'IMaj', 'SUBII/V', 'SUBV/V', 'IMaj', 'IMaj', 'II/II', 'V/II'], // Out of nowhere
+  20: ['IMaj', 'V/VI', 'iimin', 'V/II', 'iimin', 'V7', 'IMaj', 'V/VI'], // I'll be seeing you
 };
